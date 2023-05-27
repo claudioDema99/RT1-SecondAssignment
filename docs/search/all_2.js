39670cc5cd425e6aca91ae88276cff5506a523b6
claudioDema99 <5433737@studenti.unige.it>
2023-05-27 19:13:12 +0200

    docsss

--- docs/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['print_5fmessage_7',['print_message',['../namespacesubscriber.html#ac222f9b0036b7ead9c5346c9b0eafe50',1,'subscriber']]]
-];
--- docs/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['service_9',['service',['../namespaceservice.html',1,'']]],
-  ['subscriber_10',['subscriber',['../namespacesubscriber.html',1,'']]]
-];
